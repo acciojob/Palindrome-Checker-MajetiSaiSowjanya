7ac443d2395f5d945e0d8353c04984064fc5d60f
MajetiSaiSowjanya <122686287+MajetiSaiSowjanya@users.noreply.github.com>
2024-03-06 13:20:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-// complete the given function
+function checkPalindrome() {
+    // Get input string
+    const inputString = document.getElementById('inputString').value.toLowerCase();
 
-function palindrome(str){
+    // Remove non-alphanumeric characters
+    const cleanedString = inputString.replace(/[^a-z0-9]/g, '');
 
+    // Reverse the string
+    const reversedString = cleanedString.split('').reverse().join('');
+
+    // Check if the original and reversed strings are equal
+    if (cleanedString === reversedString) {
+        document.getElementById('result').textContent = 'It is a palindrome!';
+    } else {
+        document.getElementById('result').textContent = 'It is not a palindrome!';
+    }
 }
-module.exports = palindrome
